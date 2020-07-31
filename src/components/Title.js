0ce33889f2f434d2d994a7b8ab53086ba109ba82
mr-blankson <fat_tonyy@icloud.com>
2020-07-31 15:05:30 +0000

    header-component  :octocat:

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Title = () => {
+	return (
+		<div>
+			<div className="title">
+				<h1>Gredsa-16</h1>
+				<h2>Our Memories</h2>
+				<p>welcome to Gredsa-16 photo-gallery, got any image for us upload it asap</p>
+			</div>
+		</div>
+	);
+};
+
+export default Title;
